8ee428fa745b21e0e380b2988588c45fe95da186
Pikasannnnn <dong.stephen11@gmail.com>
2021-05-23 00:04:54 -0700

    Update part 3

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: sdong027
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #10  Exercise #2
+ *	Assignment: Lab #10  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -15,6 +15,7 @@
 #include "bit.h"
 #include "scheduler.h"
 
+// TIMER
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
@@ -45,6 +46,29 @@ ISR(TIMER1_COMPA_vect) {
 	}
 }
 
+// PWM
+void set_PWM(double frequency) {
+	static double current_frequency;
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; }
+		else { TCCR3B |= 0x03; }
+
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+		else if (frequency > 31250) { OCR3A = 0x0000; }
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+	}
+}
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+	set_PWM(0);
+}
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+// KEYPAD
 unsigned char GetKeypadKey() {
 	PORTC = 0xEF;	// First column
 	asm("nop");
@@ -79,8 +103,9 @@ unsigned char GetKeypadKey() {
 	return('\0');
 }
 
+// STATE_FUNCTIONS
 unsigned char lockflag = 0;
-enum ButtonStates {WAIT, LOCK_WAIT_RELEASE} B_STATES;
+enum ButtonStates {WAIT, LOCK_WAIT_RELEASE};
 int LockButton(int state) {
 	unsigned char input = ~PINB & 0x80;
 	switch (state) {
@@ -103,7 +128,7 @@ int LockButton(int state) {
 	return state;
 }
 
-enum LockStates {LOCK, LOCK_RELEASE, WAIT_PRESS, WAIT_RELEASE, UNLOCK} LED_STATES;
+enum LockStates {LOCK, LOCK_RELEASE, WAIT_PRESS, WAIT_RELEASE, UNLOCK};
 unsigned char passcode[5] = {'1','2','3','4','5'};
 int LockOut(int state) {
 	static unsigned short i;
@@ -178,13 +203,74 @@ int LockOut(int state) {
 	return state;
 }
 
+enum DB_Button_States {WAITDB, WAIT_DBRELEASE};
+unsigned char DBTrigger = 0;
+int DoorbellButton(int state) {
+	unsigned char input = ~PINA & 0x80;
+	switch (state) {
+		case WAITDB:
+			if (input) {
+				state = WAIT_RELEASE;
+				DBTrigger = 1;			
+			}
+			break;
+		case WAIT_DBRELEASE:
+			DBTrigger = 0;
+			if (!input) {
+				state = WAIT;
+			}
+			break;
+		default:
+			state = WAIT;
+			DBTrigger = 0;
+			break;			
+	}
+	return state;
+}
+enum DB_Melody_States {WAIT_PLAY, PLAY_MELODY};
+const double melody[] = {261.63, 0.00, 261.63, 0.00, 392.00, 0.00, 392.00, 0.00, 440.00, 0.00, 440.00, 0.00, 392.00, 0.00, 349.23, 0.00, 349.23, 0.00, 329.63, 0.00, 329.63, 0.00, 293.66, 0.00, 293.66, 0.00,  261.63};
+int Doorbell(int state) {
+	static short i;
+	switch (state) {
+		case WAIT_PLAY:
+			if (DBTrigger) {
+				state = PLAY_MELODY;
+				PWM_on();
+			}
+			break;
+		case PLAY_MELODY:
+			if (i >= 25) {
+				state = WAIT_PLAY;
+				PWM_off();
+				i = 0;
+			}
+			break;
+		default:
+			state = WAIT_PLAY;
+			i = 0;
+			break;			
+	}
+
+	switch (state) {
+		case WAIT_PLAY:
+			PWM_off();
+			break;
+		case PLAY_MELODY:
+			set_PWM(melody[i]);
+			i++;
+			break;		
+	}
+	return state;
+}
+
 int main(void) {
 	unsigned char x;
-	static task task1, task2;
-	task *tasks[] = {&task1, &task2};
+	static task task1, task2, task3, task4;
+	task *tasks[] = {&task1, &task2, &task3, &task4};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-	DDRB = 0x0F; PORTB = 0xF0;
+	DDRB = 0x00; PORTA = 0xFF;
+	DDRB = 0x7F; PORTB = 0x80;
 	DDRC = 0xF0; PORTC = 0x0F;
 
 	const char start = -1;
@@ -198,7 +284,17 @@ int main(void) {
 	task2.elapsedTime = task2.period;
 	task2.TickFct = &LockOut;
 
-	TimerSet(50);
+	task3.state = start;
+	task3.period = 200;
+	task3.elapsedTime = task3.period;
+	task3.TickFct = &DoorbellButton;
+
+	task4.state = start;
+	task4.period = 125;
+	task4.elapsedTime = task4.period;
+	task4.TickFct = &Doorbell;
+
+	TimerSet(25);
 	TimerOn();
 	
 	while (1) {
@@ -207,7 +303,7 @@ int main(void) {
 				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
 				tasks[i]->elapsedTime = 0;
 			}
-			tasks[i]->elapsedTime += 50;
+			tasks[i]->elapsedTime += 25;
 		}
 		while (!TimerFlag);
 		TimerFlag = 0;
